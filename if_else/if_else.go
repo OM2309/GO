3708c0311e875c4ff0602d1605d0b7a56f8454f6
Om <23anuragsharma@gmail.com>
2024-12-12 23:01:34 +0530

    loops

---
+++ if_else/if_else.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 10
+
+	// if age > 3 {
+	// 	fmt.Println("Age must be between")
+	// }
+
+	// if age > 18 {
+	// 	fmt.Println("you are 18")
+	// } else {
+	// 	fmt.Println("you are not 18")
+	// }
+
+	if age > 18 {
+		fmt.Println("you are 18")
+	} else {
+		fmt.Println("you are not 18")
+	}
+}
---
+++ switch/switch.go
@@ -0,0 +1,36 @@
+package main
+
+// import "fmt"
+
+func main() {
+	// i := 5
+
+	// switch i {
+	// case 1:
+	// 	fmt.Println(1)
+	// case 2:
+	// 	fmt.Println(2)
+	// case 3:
+	// 	fmt.Println(3)
+	// case 4:
+	// 	fmt.Println(4)
+	// default:
+	// 	fmt.Println("Other value:", i)
+	// }
+
+	// whichType := func(i interface{}) {
+	// 	switch i.(type) {
+	// 	case int:
+	// 		fmt.Println("it's an integer")
+	// 	case string:
+	// 		fmt.Println("it's a string")
+	// 	case bool:
+	// 		fmt.Println("it's a boolean")
+	// 	default:
+	// 		fmt.Println("other")
+	// 	}
+	// }
+
+	// whichType(true)
+
+}

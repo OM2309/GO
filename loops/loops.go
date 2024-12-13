eeef7b40213762b18717d07aad6df774da0c89c8
Om <23anuragsharma@gmail.com>
2024-12-13 22:57:30 +0530

    loops

--- loops/loops.go
+++ loops/loops.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// i := 1
-	// for i <= 3 {
-	// 	fmt.Println(i)
-	// 	i = i + 1
-	// }
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
 
 	// Infinite loop
 
